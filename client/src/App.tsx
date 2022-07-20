01b74d03f35f35e68f2be5c8971b019410acfd34
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-07-20 10:10:32 -0600

    fix(client): add meta and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
-import type { Component } from 'solid-js';
+import createRouter from 'router5';
+import browserPluginFactory from 'router5-plugin-browser';
+import { Component, Show } from 'solid-js';
+import { MetaProvider, Title } from 'solid-meta';
+
+const routes = [
+  { name: 'home', path: '/' },
+  { name: 'login', path: '/login' },
+];
+
+const router = createRouter(routes);
+router.usePlugin(browserPluginFactory());
+router.start();
 
 const App: Component = () => {
-  return <p class='py-20 text-center text-4xl text-green-700'>Nov8 client</p>;
+  const { name } = router.getState();
+  return (
+    <MetaProvider>
+      <Title>Home</Title>
+      <Show when={name == 'home'}>Home</Show>
+      <p class='py-20 text-center text-4xl text-green-700'>Nov8 client</p>;
+    </MetaProvider>
+  );
 };
 
 export default App;
