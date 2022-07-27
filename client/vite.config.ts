2dfc8e8083bf2fa562835ca3b1f62cf107df29b3
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-07-27 14:19:12 -0600

    build(docker): add api host to docker/vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,25 @@ import { isString } from 'remeda';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+const apiUrl = `http://${process.env.API_HOST}:${process.env.GOAPI_PORT}`;
+
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: isString(process.env.FE_PORT)
       ? parseInt(process.env.FE_PORT, 10)
       : 3000,
+    proxy: {
+      '/api': {
+        target: apiUrl,
+        changeOrigin: true,
+      },
+      '/wasm': {
+        target: apiUrl,
+        changeOrigin: true,
+      }
+    },
   },
   build: {
     target: 'esnext',
