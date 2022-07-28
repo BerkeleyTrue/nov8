c64be9524a4731e69b587f9d25a42610520d6fc3
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-07-28 09:17:43 -0600

    fix(client/pages/auth): larger w/ on larger screens

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -20,28 +20,26 @@ export const Auth: Component = () => {
             {isSignup() ? 'Register' : 'Login'}
           </h1>
         </header>
-        <div>
-          <div class='flex h-full flex-wrap items-center justify-center text-gray-800'>
-            <div class='md:w-8/12 lg:ml-20 lg:w-5/12'>
-              <form>
-                <div class='mb-6'>
-                  <input
-                    type='text'
-                    class='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
-                    placeholder='Email address'
-                  />
-                </div>
+        <div class='flex h-full flex-wrap items-center justify-center text-gray-800'>
+          <div class='2xl:w-8/12'>
+            <form>
+              <div class='mb-6'>
+                <input
+                  type='text'
+                  class='form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
+                  placeholder='Email address'
+                />
+              </div>
 
-                <button
-                  type='submit'
-                  class='inline-block w-full rounded bg-blue-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg'
-                  data-mdb-ripple='true'
-                  data-mdb-ripple-color='light'
-                >
-                  {isSignup() ? 'Register' : 'Login'}
-                </button>
-              </form>
-            </div>
+              <button
+                type='submit'
+                class='inline-block w-full rounded bg-blue-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg'
+                data-mdb-ripple='true'
+                data-mdb-ripple-color='light'
+              >
+                {isSignup() ? 'Register' : 'Login'}
+              </button>
+            </form>
           </div>
         </div>
         <div class='flex items-center justify-between'>
