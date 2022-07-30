6fea7df2f1e556cc6141dedd0797e008e9339255
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-07-29 18:47:01 -0600

    fix(game): pixi work
    Going to move away from pixi, way to hard to debug

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -5,29 +5,21 @@ import {
   Texture,
   TickerCallback,
 } from 'pixi.js';
-import { createEffect, onCleanup, ParentComponent } from 'solid-js';
+import { createEffect, onCleanup, ParentComponent, on } from 'solid-js';
 
 import { createStore } from 'solid-js/store';
 import { applyProps } from '../../lib/pixi/utils/props';
 
 export const Game: ParentComponent = () => {
+  let canvasRef: HTMLCanvasElement;
+  let containerRef: HTMLDivElement;
   let count = 0;
   const [state, setState] = createStore({
     x: 0,
     y: 0,
     rotation: 0,
-    anchor: 0,
+    anchor: 0.5,
   });
-
-  const canvasRef: HTMLCanvasElement = (
-    <canvas style={{ height: '100%', width: '100%' }} />
-  ) as any as HTMLCanvasElement;
-
-  const containerRef: HTMLDivElement = (
-    <div id='canvas-container' class='aspect-video w-full'>
-      {canvasRef}
-    </div>
-  ) as any as HTMLDivElement;
 
   const app = new Application({
     resolution: window.devicePixelRatio || 1,
@@ -38,17 +30,22 @@ export const Game: ParentComponent = () => {
 
   const container = new Container();
 
-  container.x = app.screen.width / 2;
-  container.y = app.screen.height / 2;
+  createEffect(
+    on([() => app.screen.width, () => app.screen.height], () => {
+      container.x = app.screen.width / 2;
+      container.y = app.screen.height / 2;
 
-  container.pivot.x = app.screen.width / 2;
-  container.pivot.y = app.screen.height / 2;
+      container.pivot.x = app.screen.width / 2;
+      container.pivot.y = app.screen.height / 2;
+    }),
+  );
 
   const bunny = new Sprite(
     Texture.from(
       'https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png',
     ),
   );
+
   createEffect(() => {
     container.addChild(bunny);
 
@@ -67,8 +64,8 @@ export const Game: ParentComponent = () => {
     const moveBunny: TickerCallback<any> = (delta) => {
       const i = (count += 0.05 * delta);
       setState({
-        x: Math.sin(i) * 100,
-        y: Math.cos(i) * 100,
+        x: ((1 + Math.sin(i)) / 2) * app.screen.width,
+        y: ((1 + Math.cos(i)) / 2) * app.screen.height,
         rotation: Math.sin(i) * Math.PI,
         anchor: Math.sin(i / 2),
       });
@@ -93,7 +90,15 @@ export const Game: ParentComponent = () => {
 
   return (
     <div class='game-layout h-full w-full'>
-      <main class='game-entry h-full w-full'>{containerRef}</main>
+      <main class='game-entry h-full w-full'>
+        <div
+          id='canvas-container'
+          class='aspect-video w-full'
+          ref={containerRef}
+        >
+          <canvas style={{ height: '100%', width: '100%' }} ref={canvasRef} />
+        </div>
+      </main>
     </div>
   );
 };
